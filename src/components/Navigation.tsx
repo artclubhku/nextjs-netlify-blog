cabeedce8a5330d8b66c913dd85412fddef91523
ivan0313 <ivan0313@connect.hku.hk>
2021-11-30 12:40:32 +0800

    fix: build errors

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -34,7 +34,7 @@ export default function Navigation() {
         }
       } else {
         // Scrolling Down
-        mainNav.classList.remove(['is-visible']);
+        mainNav.classList.remove('is-visible');
         if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
           mainNav.classList.add('is-fixed');
         }
@@ -56,7 +56,7 @@ export default function Navigation() {
           }
         } else {
           // Scrolling Down
-          mainNav.classList.remove(['is-visible']);
+          mainNav.classList.remove('is-visible');
           if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
             mainNav.classList.add('is-fixed');
           }
